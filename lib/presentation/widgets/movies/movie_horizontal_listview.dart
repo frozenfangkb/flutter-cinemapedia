7def8ce1c95e5fdca12d1534adb5e7c12b4d8cfd
David Rodriguez Miranda <wolfs_pride@outlook.es>
2024-07-31 09:23:46 +0200

    human format for votes number

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -101,7 +102,8 @@ class _Slide extends StatelessWidget {
                   style: textStyle.bodyMedium
                       ?.copyWith(color: Colors.yellow.shade800)),
               const SizedBox(width: 10),
-              Text('${movie.popularity}', style: textStyle.bodySmall)
+              Text(HumanFormats.number(movie.popularity),
+                  style: textStyle.bodySmall)
             ],
           )
         ],
