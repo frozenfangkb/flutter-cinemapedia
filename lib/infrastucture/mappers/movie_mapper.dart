ac5eb40e3515b5e8b2a1a172b5ea2d7a09c6d460
David Rodriguez Miranda <wolfs_pride@outlook.es>
2024-08-01 10:40:32 +0200

    add datasource, model and repository for getting movie by id

--- lib/infrastucture/mappers/movie_mapper.dart
+++ lib/infrastucture/mappers/movie_mapper.dart
@@ -1,24 +1,47 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastucture/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infrastucture/models/moviedb/movie_moviedb.dart';
 
 class MovieMapper {
   static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
-      adult: moviedb.adult,
-      backdropPath: moviedb.backdropPath != ''
-          ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
-          : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoWcWg0E8pSjBNi0TtiZsqu8uD2PAr_K11DA&s',
-      genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
-      id: moviedb.id,
-      originalLanguage: moviedb.originalLanguage,
-      originalTitle: moviedb.originalTitle,
-      overview: moviedb.overview,
-      popularity: moviedb.popularity,
-      posterPath: moviedb.posterPath != ''
-          ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
-          : 'no-poster',
-      releaseDate: moviedb.releaseDate,
-      title: moviedb.title,
-      video: moviedb.video,
-      voteAverage: moviedb.voteAverage,
-      voteCount: moviedb.voteCount);
+        adult: moviedb.adult,
+        backdropPath: moviedb.backdropPath != ''
+            ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+            : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoWcWg0E8pSjBNi0TtiZsqu8uD2PAr_K11DA&s',
+        genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
+        id: moviedb.id,
+        originalLanguage: moviedb.originalLanguage,
+        originalTitle: moviedb.originalTitle,
+        overview: moviedb.overview,
+        popularity: moviedb.popularity,
+        posterPath: moviedb.posterPath != ''
+            ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
+            : 'no-poster',
+        releaseDate: moviedb.releaseDate,
+        title: moviedb.title,
+        video: moviedb.video,
+        voteAverage: moviedb.voteAverage,
+        voteCount: moviedb.voteCount,
+      );
+
+  static Movie movieDetailsToEntity(MovieDetails movieDetails) => Movie(
+        adult: movieDetails.adult,
+        backdropPath: movieDetails.backdropPath != ''
+            ? 'https://image.tmdb.org/t/p/w500${movieDetails.backdropPath}'
+            : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoWcWg0E8pSjBNi0TtiZsqu8uD2PAr_K11DA&s',
+        genreIds: movieDetails.genres.map((e) => e.name).toList(),
+        id: movieDetails.id,
+        originalLanguage: movieDetails.originalLanguage,
+        originalTitle: movieDetails.originalTitle,
+        overview: movieDetails.overview,
+        popularity: movieDetails.popularity,
+        posterPath: movieDetails.posterPath != ''
+            ? 'https://image.tmdb.org/t/p/w500${movieDetails.posterPath}'
+            : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoWcWg0E8pSjBNi0TtiZsqu8uD2PAr_K11DA&s',
+        releaseDate: movieDetails.releaseDate,
+        title: movieDetails.title,
+        video: movieDetails.video,
+        voteAverage: movieDetails.voteAverage,
+        voteCount: movieDetails.voteCount,
+      );
 }
