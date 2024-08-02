e0d92497ae86d48decc28ca773d307cea8b67d3b
David Rodriguez Miranda <wolfs_pride@outlook.es>
2024-08-02 13:06:25 +0200

    add vote average to search

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -95,7 +96,21 @@ class _MovieItem extends StatelessWidget {
                         style: textStyles.titleSmall,
                         maxLines: 2,
                       ),
-                      Text(movie.releaseDate)
+                      Text(movie.releaseDate),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.star_half_rounded,
+                            color: Colors.yellow.shade800,
+                          ),
+                          Text(
+                            HumanFormats.number(movie.voteAverage, 1),
+                            style: textStyles.bodyMedium!.copyWith(
+                              color: Colors.yellow.shade800,
+                            ),
+                          )
+                        ],
+                      )
                     ],
                   ),
                 ),
