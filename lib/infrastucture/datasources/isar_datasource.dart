6ad10057484cad1fd729f5f4a92628584c0189b4
David Rodriguez Miranda <wolfs_pride@outlook.es>
2024-08-07 10:11:19 +0200

    add method for opening database

--- lib/infrastucture/datasources/isar_datasource.dart
+++ lib/infrastucture/datasources/isar_datasource.dart
@@ -1,7 +1,26 @@
 import 'package:cinemapedia/domain/datasources/local_storage_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 class IsarDatasource extends LocalStorageDatasource {
+  late Future<Isar> db;
+
+  IsarDatasource() {
+    db = openDB();
+  }
+
+  Future<Isar> openDB() async {
+    final dir = await getApplicationDocumentsDirectory();
+
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open([MovieSchema],
+          directory: dir.path, inspector: true);
+    }
+
+    return Future.value(Isar.getInstance());
+  }
+
   @override
   Future<bool> isMovieFavourite(int movieId) {
     // TODO: implement isMovieFavourite
@@ -9,14 +28,14 @@ class IsarDatasource extends LocalStorageDatasource {
   }
 
   @override
-  Future<List<Movie>> loadMovies({int limit = 10, int offset = 0}) {
-    // TODO: implement loadMovies
+  Future<void> toggleFavourite(Movie movie) {
+    // TODO: implement toggleFavourite
     throw UnimplementedError();
   }
 
   @override
-  Future<void> toggleFavourite(Movie movie) {
-    // TODO: implement toggleFavourite
+  Future<List<Movie>> loadMovies({int limit = 10, int offset = 0}) {
+    // TODO: implement loadMovies
     throw UnimplementedError();
   }
 }
