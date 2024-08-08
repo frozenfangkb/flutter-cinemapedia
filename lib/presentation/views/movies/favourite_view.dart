7f228649f0a6785d7b4f16c9b4f4847840a09b9f
David Rodriguez Miranda <wolfs_pride@outlook.es>
2024-08-08 08:59:49 +0200

    message in empty favourites

--- lib/presentation/views/movies/favourite_view.dart
+++ lib/presentation/views/movies/favourite_view.dart
@@ -3,6 +3,7 @@ import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class FavouriteView extends ConsumerStatefulWidget {
   const FavouriteView({super.key});
@@ -36,6 +37,28 @@ class FavouriteViewState extends ConsumerState<FavouriteView> {
   @override
   Widget build(BuildContext context) {
     final favouriteMovies = ref.watch(favouriteMoviesProvider).values.toList();
+
+    if (favouriteMovies.isEmpty) {
+      final colors = Theme.of(context).colorScheme;
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.favorite_outline_sharp, size: 60, color: colors.primary),
+            Text('¡Ups!',
+                style: TextStyle(fontSize: 30, color: colors.primary)),
+            const Text('No tienes películas favoritas',
+                style: TextStyle(fontSize: 20, color: Colors.black45)),
+            const SizedBox(height: 20),
+            FilledButton.tonal(
+              onPressed: () => context.go('/home/0'),
+              child: const Text('Empieza a buscar'),
+            )
+          ],
+        ),
+      );
+    }
 
     return Scaffold(
       body: MovieMasonry(
